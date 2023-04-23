160c65cd0bf6f03469fe6dade8a443231ea542d3
MASNA <64076575+masnaashraf@users.noreply.github.com>
2023-04-23 07:18:22 -0700

    Add files via upload

---
+++ Pub_Locations.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from matplotlib import image
+from PIL import Image
+import os
+
+
+#Load data
+df = pd.read_csv('clean_open_pubs.csv')
+
+st.title(":red[Location of all Pubs in UK 🍻🍻]")
+
+image = Image.open('image-1.jpg')
+st.image(image)
+
+
+
+
+location_type = st.selectbox(
+    "Select the location type:",
+    ('Postal Code', 'Local Authority'))
+
+if location_type == 'Postal Code':
+    location_options = df['postcode'].unique()
+    location = st.selectbox("Select the Postal Code:", location_options)
+    filtered_df = df[df['postcode'] == location]
+    st.text("Details of  Pubs based on  the Postal Code:")
+elif location_type == 'Local Authority':
+    location_options = df['local_authority'].unique()
+    location = st.selectbox("Select the Local Authority:", location_options)
+    filtered_df = df[df['local_authority'] == location]
+    st.text("Details of  Pubs based on local authority is:")
+    
+
+button_1 = st.button("Submit")
+
+if button_1:
+    st.text("Total Number of Pub:")
+    st.text(filtered_df.shape[0])
+    st.map(filtered_df)
+    st.dataframe(filtered_df)
